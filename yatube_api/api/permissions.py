86b6b8e2feb2797894593efdc5cfb65acde1ae9d
Валерия Никишина <nikishina006@gmail.com>
2026-05-12 22:37:30 +0300

    Update permissions.py

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -2,14 +2,16 @@ from rest_framework import permissions
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
+    """Allow write access only to the author of the object."""
 
     def has_permission(self, request, view):
         return (
-            request.method in permissions.SAFE_METHODS 
-                or request.user.is_authenticated
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
-        return ( request.method in permissions.SAFE_METHODS 
+        return (
+            request.method in permissions.SAFE_METHODS
             or obj.author == request.user
         )
