9c8a8d0013f0f9bc71eb00860ccd7b8fcad5a767
Vlack <nikishina006@gmail.com>
2026-05-12 22:52:40 +0300

    update 4

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path('redoc/', include('api.redoc_urls')),
+    path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),
 ]
