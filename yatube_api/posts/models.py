b790224e312315aa1713eb087947c4216a040f6f
Валерия Никишина <nikishina006@gmail.com>
2026-05-12 22:36:53 +0300

    Update models.py

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -17,16 +17,14 @@ class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
-        User, 
-        on_delete=models.CASCADE, 
-        related_name="posts"
+        User, on_delete=models.CASCADE, related_name='posts'
     )
-    image = models.ImageField(upload_to="posts/", null=True, blank=True)
+    image = models.ImageField(upload_to='posts/', null=True, blank=True)
     group = models.ForeignKey(
-        Group, 
-        on_delete=models.SET_NULL, 
-        related_name="posts", 
-        null=True, 
+        Group,
+        on_delete=models.SET_NULL,
+        related_name='posts',
+        null=True,
         blank=True
     )
 
@@ -34,19 +32,15 @@ class Post(models.Model):
         return self.text[:50]
 
     class Meta:
-        ordering = ("pub_date",)
+        ordering = ('pub_date',)
 
 
 class Comment(models.Model):
     post = models.ForeignKey(
-        Post, 
-        on_delete=models.CASCADE, 
-        related_name="comments"
+        Post, on_delete=models.CASCADE, related_name='comments'
     )
     author = models.ForeignKey(
-        User, 
-        on_delete=models.CASCADE, 
-        related_name="comments"
+        User, on_delete=models.CASCADE, related_name='comments'
     )
     text = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
@@ -57,18 +51,19 @@ class Comment(models.Model):
 
 class Follow(models.Model):
     user = models.ForeignKey(
-        User, 
-        on_delete=models.CASCADE, 
-        related_name="follower"
+        User, on_delete=models.CASCADE, related_name='follower'
     )
     following = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="following"
+        User, on_delete=models.CASCADE, related_name='following'
     )
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=["user", "following"], name="unique_follow")
+            models.UniqueConstraint(
+                fields=['user', 'following'],
+                name='unique_follow'
+            )
         ]
 
     def __str__(self):
-        return f"{self.user} follows {self.following}"
+        return f'{self.user} follows {self.following}'
